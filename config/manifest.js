23f47d0152cf49fb67cffac3070eee7210f754f1
indieNik <niki.thrill@gmail.com>
2018-05-01 21:36:26 +0530

    Added manifest icons and favicon

--- config/manifest.js
+++ config/manifest.js
@@ -18,7 +18,37 @@ module.exports = function(/* environment, appConfig */) {
         src: "images/logo.png",
         sizes: "540x540",
         type: "image/png"
-      }
+      },
+      {
+        src: "images/android-icon-36x36.png",
+        sizes: "36x36",
+        type: "image/png"
+       },
+       {
+        src: "images/android-icon-48x48.png",
+        sizes: "48x48",
+        type: "image/png"
+       },
+       {
+        src: "images/android-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png"
+       },
+       {
+        src: "images/android-icon-96x96.png",
+        sizes: "96x96",
+        type: "image/png"
+       },
+       {
+        src: "images/android-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png"
+       },
+       {
+        src: "images/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+       }
     ],
     ms: {
       tileColor: '#82fab41a'
